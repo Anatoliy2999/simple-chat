ac23943d299c32319948008a902073244fcebbf0
Anatoliy2999 <zakrevskiy.2002@bk.ru>
2017-03-15 19:31:05 +0300

    i finished do interface

--- simple-client/src/com/tolik/client/window/ClientWindow.java
+++ simple-client/src/com/tolik/client/window/ClientWindow.java
@@ -1,13 +1,41 @@
 package com.tolik.client.window;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ClientWindow extends JFrame {
     public ClientWindow() {
         setTitle("Client");
-        setSize(600,300);
+        setSize(250, 250);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        initComponents();
         setVisible(true);
+    }
+
+    private void initComponents() {
+        JPanel jPanel = new JPanel();
+        JPanel jPanel1 = new JPanel();
+        JButton send = new JButton("Send");
+        JTextField room = new JTextField();
+        JTextField inPut = new JTextField();
+        JLabel labelRoom = new JLabel("Room");
+        JLabel labelName = new JLabel("Nick name");
+        JTextField name = new JTextField();
+        JButton connect = new JButton("Connect");
+
+        jPanel.setLayout(new GridLayout(1, 2));
+        jPanel.add(inPut);
+        jPanel.add(send);
+
+        jPanel1.setLayout(new GridLayout(3, 2));
+        jPanel1.add(labelRoom);
+        jPanel1.add(room);
+        jPanel1.add(labelName);
+        jPanel1.add(name);
+        jPanel1.add(connect);
+
+        add(jPanel, BorderLayout.PAGE_END);
+        add(jPanel1, BorderLayout.PAGE_START);
     }
 }
--- simple-server/src/com/tolik/server/window/ServerWindow.java
+++ simple-server/src/com/tolik/server/window/ServerWindow.java
@@ -1,13 +1,32 @@
 package com.tolik.server.window;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class ServerWindow extends JFrame{
+public class ServerWindow extends JFrame {
     public ServerWindow() {
         setTitle("Server");
-        setSize(600,300);
+        setSize(250, 250);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        initComponents();
         setVisible(true);
+    }
+
+    private void initComponents() {
+        JButton openRoom = new JButton("Open Room");
+        JLabel labelRoom = new JLabel("World");
+
+        String[] massage = {"1", "2", "2", "2", "2"};
+
+        JScrollPane scrollPane = new JScrollPane();
+        JList<String> jMassage = new JList<String>(massage);
+
+        scrollPane.setViewportView(jMassage);
+        jMassage.setLayoutOrientation(JList.VERTICAL);
+
+        add(labelRoom, BorderLayout.PAGE_START);
+        add(jMassage, BorderLayout.CENTER);
+        add(openRoom, BorderLayout.PAGE_END);
     }
 }
